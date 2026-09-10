43dd9a54ee5580d363634cfcf8077ad671acc1b9
thayssaalexandre <thayssa.alexandre@edu.senai.br>
2026-09-10 15:32:58 -0300

    Exer2

---
+++ Carro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    internal class Carro
+    {
+        private int _VELOCIDADEATUAL = 60;
+
+        public void Acelerar()
+        {
+            _VELOCIDADEATUAL += 10;
+            Console.WriteLine($"Velocidade: {VelocidadeAtual} KM/H");
+        }
+
+        public void Frear()
+        {
+            if (_VELOCIDADEATUAL <= 0)
+            {
+                Console.WriteLine("Carro Parado");
+            }
+            else
+            {
+                _VELOCIDADEATUAL -= 10;
+                Console.WriteLine($"Velocidade: {VelocidadeAtual} KM/H");
+            }
+        }
+        public int VelocidadeAtual
+        {
+            get {  return _VELOCIDADEATUAL; }
+        }
+    }
+}
