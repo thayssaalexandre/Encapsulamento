8ea09550db15309e7070c6b7115c1abb1719eeb6
thayssaalexandre <thayssa.alexandre@edu.senai.br>
2026-09-10 15:02:20 -0300

    Exer1

---
+++ Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    public class Pessoa
+    {
+        private string _nome;
+
+        public string Nome
+        {
+
+            get { return _nome; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Não reconhecido");
+                }
+                else
+                {
+                    _nome = value;
+                }
+            }
+        }
+    }
+}
