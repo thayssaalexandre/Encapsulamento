7bf494a899b3fa2050ae67ed101ec21a0bfa2d9d
thayssaalexandre <thayssa.alexandre@edu.senai.br>
2026-09-11 07:49:57 -0300

    Exer 2

---
+++ Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    internal class Produto
+    {
+        private double _Preco;
+
+        public double Preco
+        {
+            get { return _Preco; }
+
+            set
+            {
+                if (Preco <= 0)
+                {
+                    Console.WriteLine("Preço: R$0,00");
+                }
+                else
+                {
+                    _Preco = value;
+                }
+            }
+        }
+    }
+}
