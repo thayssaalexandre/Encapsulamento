65e49bb3d5befb8d6efe6f68eec26b127bf6b047
thayssaalexandre <thayssa.alexandre@edu.senai.br>
2026-09-11 08:13:14 -0300

    Exer 4

---
+++ Termometro.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    internal class Termometro
+    {
+
+        private double _temperaturaCelcius;
+
+        public double f;
+
+        public double k;
+
+        public double celcius
+
+
+        {
+            get { return _temperaturaCelcius; }
+            set
+            {
+                _temperaturaCelcius = value;
+
+            }
+        }
+        public double fahrenheit
+
+        {
+            get
+            {
+                return (_temperaturaCelcius * 9 / 5) + 32;
+
+            }
+            set
+            {
+                _temperaturaCelcius = (value - 32) * 5 / 9;
+
+            }
+        }
+        public double kelvin
+
+        {
+            get
+            {
+                return _temperaturaCelcius = celcius + 273.15;
+
+            }
+            set
+            {
+
+                _temperaturaCelcius = value - 273.15;
+
+            }
+        }
+    }
+}
